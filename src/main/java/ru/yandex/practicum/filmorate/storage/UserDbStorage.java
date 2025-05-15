63a2133c8aad674d7bcc53cf0f0062a05c3d8266
Natalya Abramenko <natalya-gospodinova@yandex.ru>
2025-05-15 13:01:47 +0300

    Исправить замечания по результатам ревью по ТЗ к спринту 12 (диаграмма)

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserDbStorage.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.storage;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
